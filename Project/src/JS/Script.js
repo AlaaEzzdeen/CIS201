24bc03489c48ddc6cf7ee928f2ae58c954e986b9
AlaaEzzdeen <alaa.fareed1234@gmail.com>
2022-05-21 23:59:39 +0300

    Finished adding products

--- Project/src/JS/Script.js
+++ Project/src/JS/Script.js
@@ -9,21 +9,19 @@ var GPU = [
 
 var CPU =[
 
-    {header: "i9 3090", img: "../src/imgs/nvidia-rtx-3090-gpu.jpg", description: "The most Powerful GPU of all time till now", Price: "1200$", Cart: "Add To Cart"},
-    {header: "i7 3090", img: "../src/imgs/nvidia-rtx-3090-gpu.jpg", description: "The most Powerful GPU of all time till now", Price: "1200$", Cart: "Add To Cart"},
-    {header: "i5 3090", img: "../src/imgs/nvidia-rtx-3090-gpu.jpg", description: "The most Powerful GPU of all time till now", Price: "1200$", Cart: "Add To Cart"}
+    {header: "Intel i9 12900k", img: "../src/imgs/i9.png", description: "The Intel Core i9-12900K is a high end CPU for desktops based on the Alder Lake architecture. And offers 8 performance cores and 8 efficiency cores. The performance cores (P-cores) support HyperThreading, leading to 24 Threads that can be processed at once.", Price: "589$", Cart: "Add To Cart"},
+    {header: "Intel i7 12700k", img: "../src/imgs/i7.png", description: " i7 12700k processor has a 5 GHz Turbo Boost Max 3.0 frequency with 25MB of cache for fast and reliable performance. The Core i7-12700K also includes support for up to PCI Express 5.0 and dual-channel DDR5 memory at 4800 MHz to help run and multitask a variety of demanding applications.", Price: "409$", Cart: "Add To Cart"},
+    {header: "Intel i5 12600k", img: "../src/imgs/i5.png", description: "The Intel Core i5-12600K is a brilliant processor for gaming. Rocking 10 physical hybrid cores and 16 threads, this CPU is more than powerful enough for the latest GPUs and PC games", Price: "320$", Cart: "Add To Cart"},
+    {header: "AMD Ryzen 5 5600x", img: "../src/imgs/5600x.jpg", description: "This CPU features six cores, twelve threads, and a boost to 4.6GHz.", Price: "299$", Cart: "Add To Cart"},
+    {header: "AMD Ryzen 7 5800x", img: "../src/imgs/5800x.png", description: "The 5800X is one of the best CPUs for gaming, and ideally suited for a mid-range setup. It is an 8-core/16-thread part with a 3.8GHz base clock, 4.7GHz max boost clock, and 32MB of L3 cache. A great all-around workhorse, it can power both your gaming and productivity needs ", Price: "449$", Cart: "Add To Cart"}
 ];
 
 var Mobo =[
-
-    {header: "B Item One", img: "01.png", description: "Blah blah"}, 
-    {header: "B Item two", img: "01.png", description: "Blah blah"}
-];
-
-var PSU =[
-
-    {header: "B Item One", img: "01.png", description: "Blah blah"}, 
-    {header: "B Item two", img: "01.png", description: "Blah blah"}
+    {header: "GIGABYTE H610M", img: "../src/imgs/h610.png", description: "Intel LGA 1700 Socket: Supports 12th Gen Intel Core Series Processors with DDR4 support", Price: "89.99$", Cart: "Add To Cart"},
+    {header: "Gigabyte Z690 Aorus Master", img: "../src/imgs/z690.png", description: "Support 12th Gen Intel Series Processors with DDR5 memory", Price: "449.99$", Cart: "Add To Cart"},
+    {header: "MSI MAG B660 Tomahawk", img: "../src/imgs/5800x.png", description: "Intel LGA 1700 Socket: Supports 12th Gen Intel Core Series Processors with DDR4 support", Price: "189.99$", Cart: "Add To Cart"},
+    {header: "GIGABYTE A520M", img: "../src/imgs/A520.png", description: "MicroATX, Supports AMD Ryzen 5000 Series/ 3rd Gen Ryzen and 3rd Gen Ryzen with Radeon Graphics Processors", Price: "189.99$", Cart: "Add To Cart"},
+    {header: "MSI B550-A PRO", img: "../src/imgs/5800x.png", description: "Support for 3rd Gen AMD Ryzen processors and future AMD Ryzen processors with BIOS update", Price: "139.99$", Cart: "Add To Cart"}
 ];
 
 
